dc51a8b5b9bd7ce5829c69acb6d5a63f14211952
Luisa Fernanda Acosta Vergel <lfacostav@ufpso.edu.co>
2023-05-19 20:09:31 -0500

    Actualizar

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { ref } from 'vue'
+const title = ref('Ejercicio')
+
+  
+
+</script>
+
+<template>
+  <div>
+    <ul>
+      <h2>{{ title }}</h2>
+      <input type="text" v-model="title" />
+      <button @click="addTarea">Crear tarea</button>
+    </ul>
+  </div>
+</template>
+
+<style scoped>
+ul {
+  width: 420px;
+  margin: 0 auto;
+  background: gainsboro;
+  border-radius: 8px;
+}
+
+</style>
